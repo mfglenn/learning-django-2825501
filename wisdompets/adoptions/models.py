9c58d54fa7a8b4f25381c410ce6227cd7fd1ca86
Matthew Glenn <mfglenn@outlook.com>
2020-11-03 15:03:20 -0700

    Add the management commands to the adoptions app in order to import the base data set from the pet_data.csv.  Correct error in the models.py by changing the class name from Vaccines to Vaccine, and updating the Many to Many relationship.

--- wisdompets/adoptions/models.py
+++ wisdompets/adoptions/models.py
@@ -13,7 +13,7 @@ class Pet(models.Model):
     sex = models.CharField(max_length=1, choices=SEX_CHOICES, blank=True)
     submission_date = models.DateTimeField()
     age = models.IntegerField(null=True)
-    vaccinations = models.ManyToManyField('Vaccines', blank=True)
+    vaccinations = models.ManyToManyField('Vaccine', blank=True)
 
-class Vaccines(models.Model):
+class Vaccine(models.Model):
     name = models.CharField(max_length=50)
