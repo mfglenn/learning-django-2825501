78befc7dde8684c51dcbdebc9397f25f302289d2
Matthew Glenn <mfglenn@outlook.com>
2020-11-03 16:20:05 -0700

    Update the urls.py and views.py files to control the routing of the site to the adoption application.  Add the templates required to support the new views.py configuration.

--- wisdompets/adoptions/views.py
+++ wisdompets/adoptions/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from django.http import Http404
 
-# Create your views here.
+#Import Models for Queries
+from .models import Pet
+
+# Return the Home View
+def home(request):
+    pets = Pet.objects.all()
+    return render(request, 'home.html', {
+        'pets': pets,
+    })
+
+# Return the Pet Detail
+def pet_detail(request, pet_id):
+    try:
+        pet = Pet.objects.get(id=pet_id)
+    except Pet.DoesNotExist:
+        raise Http404('Pet not found')
+
+    return render(request, 'pet_detail.html', {
+        'pet': pet,
+    })
