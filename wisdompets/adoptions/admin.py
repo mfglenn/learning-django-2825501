ab3a414fa107adedf22ff6ff9839363ba279e56b
Matthew Glenn <mfglenn@outlook.com>
2020-11-03 15:15:10 -0700

    Add an admin interface for the Pet model.

--- wisdompets/adoptions/admin.py
+++ wisdompets/adoptions/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Pet
+
+#Pet Admin Interface
+@admin.register(Pet)
+class PetAdmin(admin.ModelAdmin):
+    pass #direct inheritance
